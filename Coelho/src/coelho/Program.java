7bb517bf7f7d8c357931fdde2ee7855eb84afd70
Ricardo <cat-richard@hotmail.com>
2023-12-07 10:20:58 -0300

    codigo

--- Coelho/src/coelho/Program.java
+++
@@ -1,8 +0,0 @@
-package coelho;
-
-
-public class Program {
-    public static void main(String[] args) {
-        App.menu();
-    }
-}
---
+++ semana 04/Reembolso.java
@@ -0,0 +1,27 @@
+
+
+
+
+public class Reembolso {
+    private int idFatura;
+    private double valor;
+    private Calendar data;
+
+    public Reembolso(int idFatura, double valor, Calendar data) {
+        this.idFatura = idFatura;
+        this.valor = valor;
+        this.data = data;
+    }
+
+    public int getIdFatura() {
+        return idFatura;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public Calendar getData() {
+        return data;
+    }
+}
